ed9014a01ff8a858ce21149efbe24a4b7bf3af05
Paulin Bougault-Bourges <paulin.bougault@yahoo.fr>
2023-02-27 15:47:14 +0100

    validations added

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,7 @@
 class Planet < ApplicationRecord
   belongs_to :user
   has_many :reservations
+  validates :name, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :price, persence: true
 end
