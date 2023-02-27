8b24b8f6064bb31ac0d1fbf0ad309d5d0b9f07c9
Guillaume Bonjean <gbonjean@free.fr>
2023-02-27 14:10:31 +0100

    push base corrigé

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :planets
+  has_many :reservations, through: :planets
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ db/migrate/20230227124905_create_planets.rb
@@ -0,0 +1,12 @@
+class CreatePlanets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :planets do |t|
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.references :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
