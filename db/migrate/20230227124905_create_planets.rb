edb3739602e288459e93827b701cb5989e988349
Guillaume Bonjean <gbonjean@free.fr>
2023-02-27 19:13:48 +0100

    adapted planet model, added cloudinary

--- db/migrate/20230227124905_create_planets.rb
+++ db/migrate/20230227124905_create_planets.rb
@@ -3,7 +3,9 @@ class CreatePlanets < ActiveRecord::Migration[7.0]
     create_table :planets do |t|
       t.string :name
       t.text :description
-      t.integer :price
+      t.integer :price_per_night
+      t.integer :rating
+      t.string :picture_url
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
